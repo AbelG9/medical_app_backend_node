4b4672897e31c7897a4385ed7a7a3054b30f7e8e
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-09-16 18:30:44 -0500

    installed morgan and configure for requests

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 import { api as routerApi } from "./prisma/router/patient.routes.js";
 
 const app = express();
 const port = 3000;
+
+const errorHandler = (error, req, res, next) => {
+  res.status(400).json({
+    message: "Error while requesting the operation",
+    content: error.message,
+  });
+};
+
+app.use(morgan("common"));
+const PORT = process.env.PORT;
+app.use(express.json());
+app.use(errorHandler);
 
 app.use(`/api/v1`, routerApi);
 
